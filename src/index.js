25edc79c473ff0abf910a330e808ee263901db96
Gabriel Timba <gabrieltimba@hotmail.com>
2022-03-13 20:45:43 +0200

    add:create users

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const {v4:uuidv4} = require("uuid");
 
 // const { v4: uuidv4 } = require('uuid');
 
@@ -8,14 +9,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+
+  const user=({
+    id:uuidv4(),
+    name,
+    username,
+    todos:[]
+  })
+
+  users.push(user);
+
+  return response.status(201).json({...user})
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
